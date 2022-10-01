9eab88a0cb196ffe54f3260383a1cedb78c2afca
vencerConsul <o.lermovenz@gmail.com>
2022-10-01 17:39:49 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
 @section('content')
     <div class="container-fluid vh-100 home">
         <div class="d-flex justify-content-center align-items-center vh-100">
-            <div class="h-50">
+            <div class="h-50 login">
                 <div class="card py-5 px-5">
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
                             <div class="form-group mb-3">
                                 <label for="email" class="col-form-label">{{ __('Username') }}</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Username>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Username">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
