9d1566581e556835952b135a63db0ce3213c84b4
vencerConsul <o.lermovenz@gmail.com>
2022-10-01 17:13:10 +0800

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!App::environment(['local', 'staging'])) {
+            $this->app['request']->server->set('HTTPS', true);
+            URL::forceScheme('https');
+        }
+        
+        if (!app()->environment(['local', 'staging'])) {
+            $this->app['request']->server->set('HTTPS', true);
+            URL::forceScheme('https');
+        }
     }
 }
