5001481aef9ff8f28ded95c64682e40390df45a6
vencerConsul <o.lermovenz@gmail.com>
2022-10-20 19:14:35 +0800

    small changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,14 +157,6 @@
 
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in shadow"
                             aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-tertiary"></i>
-                                Profile
-                            </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-tertiary"></i>
-                                Activity Log
-                            </a>
                             <div class="dropdown-divider gray-900"></div>
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-tertiary"></i>
