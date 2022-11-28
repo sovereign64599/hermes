3f6c237e25caeb9384ce6b5a78644cc48483384c
vencerConsul <o.lermovenz@gmail.com>
2022-11-28 16:03:30 +0800

    please gumana kana po huhuhuhu

--- app/Http/Controllers/Admin/ItemsController.php
+++ app/Http/Controllers/Admin/ItemsController.php
@@ -209,12 +209,12 @@ class ItemsController extends Controller
             return back()->with('success', ucfirst($request->item_name). ' already exist. Item quantity updated.');
         }
 
-        $like = scandir('storage/img/item_photo');
+        $like = scandir('img/item_photo');
         foreach ($like as $thisFile) {
             $rs = Items::where('item_photo', $thisFile)->first();
             if (!$rs) {
                 if($thisFile != "." and $thisFile != ".."){
-                        unlink('storage/img/item_photo/' . $thisFile);
+                        unlink('public/img/item_photo/' . $thisFile);
                 }
             }
         }
@@ -271,12 +271,12 @@ class ItemsController extends Controller
                 'item_barcode.regex' => 'Barcode format must be 00-00-000000',
             ]);
 
-            $like = scandir('storage/img/item_photo');
+            $like = scandir('img/item_photo');
             foreach ($like as $thisFile) {
                 $rs = Items::where('item_photo', $thisFile)->first();
                 if (!$rs) {
                     if($thisFile != "." and $thisFile != ".."){
-                            unlink ('storage/img/item_photo/' . $thisFile);
+                            unlink ('public/img/item_photo/' . $thisFile);
                     }
                 }
             }
