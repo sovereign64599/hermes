58960ede438ef60847caa3c65d070bd21d188f49
vencerConsul <o.lermovenz@gmail.com>
2022-11-30 14:22:02 +0800

    small changes

--- app/Http/Controllers/Admin/ItemsController.php
+++ app/Http/Controllers/Admin/ItemsController.php
@@ -272,7 +272,7 @@ class ItemsController extends Controller
                 'item_barcode.regex' => 'Barcode format must be 00-00-000000',
             ]);
 
-            $like = scandir('storage/img/item_photo');
+            $like = scandir('public/img/item_photo');
             foreach ($like as $thisFile) {
                 $rs = Items::where('item_photo', $thisFile)->first();
                 if (!$rs) {
